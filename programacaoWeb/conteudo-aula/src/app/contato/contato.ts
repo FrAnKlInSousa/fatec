59aa536107cb1fe94e21d370f7f859d0c54cfcc7
franklin <frank.fkn.23@gmail.com>
2026-04-02 13:54:27 -0300

    criando pagina de contato

--- programacaoWeb/conteudo-aula/src/app/contato/contato.ts
+++ programacaoWeb/conteudo-aula/src/app/contato/contato.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Cliente } from '../models/cliente';
+import { tick } from '@angular/core/testing';
 
 @Component({
   selector: 'app-contato',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contato.html',
   styleUrl: './contato.css',
 })
 export class Contato {
-  public alerta: string = '';
+  public nome: string = '';
+  public email: string = '';
+  public assunto: string = '';
+  public telefone: string = '';
+  public mensagem: string = '';
+  public texto: string = '';
 
+  public obj: Cliente = new Cliente();
   public enviar() {
-    this.alerta = 'Email enviado.';
+    this.mensagem = 'Email enviado com sucesso.';
+    let data = `Email: ${this.email}, Assunto: ${this.assunto}, Mensagem: ${this.mensagem}`;
+
+    this.obj.nome = this.nome;
+    this.obj.email = this.email;
+    this.obj.telefone = this.telefone;
+    localStorage.setItem('mensagem', data);
+    localStorage.setItem('cliente', JSON.stringify(this.obj));
   }
 }
