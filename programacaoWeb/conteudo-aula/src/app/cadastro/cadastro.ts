cf3065cf697cf40f177edde143da4409a7ba7606
franklin <frank.fkn.23@gmail.com>
2026-04-02 14:04:25 -0300

    criando classe produto

--- programacaoWeb/conteudo-aula/src/app/cadastro/cadastro.ts
+++ programacaoWeb/conteudo-aula/src/app/cadastro/cadastro.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Cliente } from '../models/cliente';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.html',
   styleUrl: './cadastro.css',
 })
-export class Cadastro {}
+export class Cadastro {
+  nome: string = '';
+  email: string = '';
+  telefone: string = '';
+
+  cadastrar(){
+    this.nome = 
+  }
+}
