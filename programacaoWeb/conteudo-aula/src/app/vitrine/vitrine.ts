c16e716110c58f5ff1821f67f70c77517bc4c2d9
franklin <frank.fkn.23@gmail.com>
2026-04-06 12:36:48 -0300

    adicionando item na cesta

--- programacaoWeb/conteudo-aula/src/app/vitrine/vitrine.ts
+++ programacaoWeb/conteudo-aula/src/app/vitrine/vitrine.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Produto } from '../models/produto';
+import { ItemCesta } from '../models/item-cesta';
 
 @Component({
   selector: 'app-vitrine',
@@ -103,4 +104,27 @@ export class Vitrine {
       keywords: 'carrinho, mao, obra, construcao, transporte',
     },
   ];
+
+  redirecionar(obj: Produto) {
+    localStorage.setItem('ProdutoSelecionado', JSON.stringify(obj));
+    location.href = 'detalhe';
+  }
+
+  adicionarItemCesta(obj: Produto) {
+    let json = localStorage.getItem('cesta');
+    let lista: ItemCesta[] = [];
+    let item: ItemCesta = new ItemCesta();
+
+    item.produto = obj;
+    item.quantidade = 1;
+    item.valor = obj.valor;
+
+    if (json != null) {
+      lista = JSON.parse(json);
+    }
+
+    lista.push(item);
+    localStorage.setItem('cesta', JSON.stringify(lista));
+    location.href = 'cesta';
+  }
 }
