1d4402cb1f6751911455d5a90601f795bbee9df8
franklin <frank.fkn.23@gmail.com>
2026-04-06 11:24:02 -0300

    ajustes contato.ts - aula

--- programacaoWeb/conteudo-aula/src/app/contato/contato.ts
+++ programacaoWeb/conteudo-aula/src/app/contato/contato.ts
@@ -11,22 +11,30 @@ import { tick } from '@angular/core/testing';
   styleUrl: './contato.css',
 })
 export class Contato {
-  public nome: string = '';
-  public email: string = '';
-  public assunto: string = '';
-  public telefone: string = '';
   public mensagem: string = '';
+  public titulo: string = '';
+  public nome: string = '';
+  public telefone: string = '';
+  public email: string = '';
   public texto: string = '';
+  public copia: string = '';
 
   public obj: Cliente = new Cliente();
+
   public enviar() {
     this.mensagem = 'Email enviado com sucesso.';
-    let data = `Email: ${this.email}, Assunto: ${this.assunto}, Mensagem: ${this.mensagem}`;
+
+    let data = `Título: ${this.titulo},
+    Nome: ${this.nome}, Email: ${this.email},
+    Copia: ${this.copia},
+    Telefone: ${this.telefone},
+    Texto: ${this.texto}`;
 
     this.obj.nome = this.nome;
     this.obj.email = this.email;
     this.obj.telefone = this.telefone;
-    localStorage.setItem('mensagem', data);
+
+    localStorage.setItem('mensagemContato', data);
     localStorage.setItem('cliente', JSON.stringify(this.obj));
   }
 }
