31125fd549778b54dfee7a71a00a5d7c69f2a937
franklin <frank.fkn.23@gmail.com>
2026-04-06 21:10:39 -0300

    pagina cesta

--- programacaoWeb/conteudo-aula/src/app/cesta/cesta.ts
+++ programacaoWeb/conteudo-aula/src/app/cesta/cesta.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Produto } from '../models/produto';
+import { ItemCesta } from '../models/item-cesta';
 
 @Component({
   selector: 'app-cesta',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cesta.html',
   styleUrl: './cesta.css',
 })
-export class Cesta {}
+export class Cesta {
+  mensagem: string = '';
+  lista: ItemCesta[] = [];
+  total: number = 0;
+
+  ngOnInit(){
+    let json = localStorage.getItem('cesta')
+    if (json != null){
+      this.lista = JSON.parse(json);
+      this.calculaTotal();
+    }else{
+      this.mensagem = 'Sua cesta está vazia!'
+    }
+  }
+
+  calculaTotal(){
+    this.total = 0;
+    for(let obj of this.lista){
+      this.total = this.total + obj.valor
+    }
+  }
+}
