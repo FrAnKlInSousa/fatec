8026780d004a0060ca3110854c799a49b3c0a397
franklin <frank.fkn.23@gmail.com>
2026-04-15 21:34:14 -0300

    classe chamado

--- rec01/rec/src/app/models/chamado.ts
+++ rec01/rec/src/app/models/chamado.ts
@@ -1,1 +1,13 @@
-export class Chamado {}
+export class Chamado {
+  codigo: number;
+  assunto: string;
+  prioridade: string;
+  detalhes: string;
+
+  constructor(codigo: number, assunto: string, prioridade: string, detalhes: string) {
+    this.codigo = codigo;
+    this.assunto = assunto;
+    this.prioridade = prioridade;
+    this.detalhes = detalhes;
+  }
+}
