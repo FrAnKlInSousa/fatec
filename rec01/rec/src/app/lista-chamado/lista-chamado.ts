722d2e8d26d9226691bf85094ea237a0d1ac76be
franklin <frank.fkn.23@gmail.com>
2026-04-15 22:23:55 -0300

    listar chamados

--- rec01/rec/src/app/lista-chamado/lista-chamado.ts
+++ rec01/rec/src/app/lista-chamado/lista-chamado.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Chamado } from '../models/chamado';
 
 @Component({
   selector: 'app-lista-chamado',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './lista-chamado.html',
   styleUrl: './lista-chamado.css',
 })
-export class ListaChamado {}
+export class ListaChamado {
+  lista: Chamado[] = [];
+
+  ngOnInit() {
+    this.listarChamados();
+  }
+
+  listarChamados() {
+    const chamados = localStorage.getItem('chamados');
+
+    if (chamados) {
+      this.lista = JSON.parse(chamados);
+    }
+  }
+}
