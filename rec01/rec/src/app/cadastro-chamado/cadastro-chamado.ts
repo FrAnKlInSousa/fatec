88abd7ef515c7679f185ad75b375a7c08e2f9b15
franklin <frank.fkn.23@gmail.com>
2026-04-15 21:57:11 -0300

    cadastro chamado

--- rec01/rec/src/app/cadastro-chamado/cadastro-chamado.ts
+++ rec01/rec/src/app/cadastro-chamado/cadastro-chamado.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Chamado } from '../models/chamado';
 
 @Component({
   selector: 'app-cadastro-chamado',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro-chamado.html',
   styleUrl: './cadastro-chamado.css',
 })
-export class CadastroChamado {}
+export class CadastroChamado {
+  codigo: number | null = null;
+  assunto: string = '';
+  prioridade: string = '';
+  detalhes: string = '';
+
+  gravarChamado() {
+    const chamado = new Chamado(this.codigo ?? 0, this.assunto, this.prioridade, this.detalhes);
+    localStorage.setItem('cadastro', JSON.stringify(chamado));
+    location.href = '';
+  }
+}
