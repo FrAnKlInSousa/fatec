056a0964e56dcb620bb0d8d21b3a06931d6bbabb
franklin <frank.fkn.23@gmail.com>
2026-04-24 10:50:32 -0300

    criando getters e setters para cliente e pedido

--- programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/Cliente.java
+++ programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/Cliente.java
@@ -1,9 +1,40 @@
 package fatec.ads.loja;
 
 public class Cliente {
-    String nome;
-    String email;
-    String telefone;
-    String logradouro;
-    String documento;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String logradouro;
+    private String documento;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getLogradouro() {
+        return logradouro;
+    }
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+    public String getDocumento() {
+        return documento;
+    }
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
 }
--- programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/Pedido.java
+++ programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/Pedido.java
@@ -1,7 +1,27 @@
 package fatec.ads.loja;
 
 public class Pedido {
-    Produto produto;
-    int quantidade;
-    double valor;
+    private Produto produto;
+    private int quantidade;
+    private double valor;
+    
+    public Produto getProduto() {
+        return produto;
+    }
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+    public int getQuantidade() {
+        return quantidade;
+    }
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
+
