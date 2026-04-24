f276e2793deada1aa89606e40e40cd90197e49b5
franklin <frank.fkn.23@gmail.com>
2026-04-24 12:51:05 -0300

    endpoints cliente

---
+++ programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/ClienteController.java
@@ -0,0 +1,48 @@
+package fatec.ads.loja;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+
+@RestController
+@CrossOrigin
+public class ClienteController {
+
+Cliente cliente;
+
+@Autowired
+Repo repo;
+
+@PostMapping("/cliente")
+public Cliente criar(@RequestBody Cliente cliente) {
+    repo.salvar(cliente);
+    return cliente;
+}
+
+@GetMapping("/clientes")
+public List<Cliente> getMethodName() {
+    return repo.listaClientes();
+}
+
+@GetMapping("/cliente/{email}")
+public Cliente getMethodName(@PathVariable String email) {
+    return repo.buscaEmail(email);
+}
+
+@PutMapping("/cliente/{email}")
+public Cliente putMethodName(@PathVariable String email, @RequestBody Cliente cliente) {
+    repo.editar(cliente, email);
+    return cliente;
+}
+
+}
