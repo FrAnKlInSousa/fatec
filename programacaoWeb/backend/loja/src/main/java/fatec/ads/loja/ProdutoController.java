4951935cbb22d829c4505738b4cd2e1f15db2501
franklin <frank.fkn.23@gmail.com>
2026-04-24 10:38:38 -0300

    removendo imports não utilizados

--- programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/ProdutoController.java
+++ programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/ProdutoController.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
