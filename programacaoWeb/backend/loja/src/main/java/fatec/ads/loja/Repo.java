61f13bbce9f2d02b4209e05b232db9e8a62b8b55
franklin <frank.fkn.23@gmail.com>
2026-04-24 12:49:43 -0300

    classe repositorio

---
+++ programacaoWeb/backend/loja/src/main/java/fatec/ads/loja/Repo.java
@@ -0,0 +1,47 @@
+package fatec.ads.loja;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Repo {
+
+    private List<Cliente> clientes = new ArrayList<>();
+
+    public void salvar(Cliente cliente){
+        if (buscaEmail(cliente.getEmail()) == null){
+            clientes.add(cliente);
+        }
+    }
+    public void editar(Cliente cliente, String email){
+        for(Cliente c: clientes){
+            System.out.println(c.getEmail() + " >>> " + email);
+            if (c.getEmail().equals(email)){
+                c.setDocumento(cliente.getDocumento());
+                c.setEmail(cliente.getEmail());
+                c.setLogradouro(cliente.getLogradouro());
+                c.setNome(cliente.getNome());
+                c.setTelefone(cliente.getTelefone());
+                break;
+            }
+        }
+
+        
+    }
+
+    public Cliente buscaEmail(String email){
+        for(Cliente c: clientes){
+            if (c.getEmail().equals(email)){
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public List<Cliente> listaClientes(){
+        return clientes;
+    }
+
+}
