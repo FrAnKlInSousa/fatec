d133da5d9ee267481ad6dea8342fbe6b5306fa8b
franklin <frank.fkn.23@gmail.com>
2026-04-25 13:32:43 -0300

    multiplicacao e subtracao

--- algoritmos/src/main/java/com/example/algoritmos/Calculadora.java
+++ algoritmos/src/main/java/com/example/algoritmos/Calculadora.java
@@ -6,13 +6,45 @@ public class Calculadora {
     public float soma(float termoA, float termoB){
         return termoA + termoB;
     }
+
+    public float subtrai(float termoA, float termoB){
+        return termoA - termoB;
+    }
+
+    public float multiplica(float termoA, float termoB){
+        return termoA * termoB;
+    }
     public static void main(String[] args) {
         Calculadora calc = new Calculadora();
-        System.out.println("Informe os números para serem somados: ");
+        int operação;
+        float termoA, termoB;
+        System.out.println("Informe a operação desejada (1-Somar, 2-subtrair, 3-multiplicar, 4-dividir)");
         Scanner entrada = new Scanner(System.in);
-        float termoA = entrada.nextFloat();
-        float termoB = entrada.nextFloat();
-        System.out.printf("A soma entre %.2f e %.2f é = %.2f\n", termoA, termoB, calc.soma(termoA, termoB));
+        operação = entrada.nextInt();
+        switch (operação) {
+            case 1:
+                System.out.println("Informe os números para serem somados: ");
+                termoA = entrada.nextFloat();
+                termoB = entrada.nextFloat();
+                System.out.printf("A soma entre %.2f e %.2f é = %.2f\n", termoA, termoB, calc.soma(termoA, termoB));
+                
+                break;
+            case 2:
+                System.out.println("Informe os números para serem subtraídos (A - B): ");
+                termoA = entrada.nextFloat();
+                termoB = entrada.nextFloat();
+                System.out.printf("A diferença entre %.2f e %.2f é = %.2f\n", termoA, termoB, calc.soma(termoA, termoB));
+                break;
+            case 3:
+                System.out.println("Informe os números para serem multiplicados: ");
+                termoA = entrada.nextFloat();
+                termoB = entrada.nextFloat();
+                System.out.printf("O produto dos números %.2f e %.2f é = %.2f\n", termoA, termoB, calc.soma(termoA, termoB));
+                break;
+        
+            default:
+                break;
+        }
 
         entrada.close();
     }
