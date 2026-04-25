8ea1d7f06d2e9d51369fedc7bc7b071c601c2861
franklin <frank.fkn.23@gmail.com>
2026-04-25 13:08:09 -0300

    teste soma

--- algoritmos/src/test/java/com/example/algoritmos/AlgoritmosApplicationTests.java
+++ algoritmos/src/test/java/com/example/algoritmos/AlgoritmosApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.algoritmos;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AlgoritmosApplicationTests {
+	private Calculadora calc;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	void setup(){
+		calc = new Calculadora();
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"2, 3, 5",
+		"40, 23, 63",
+		"0, 0, 0"
+	})
+	void soma(float termoA, float termoB, float expected) {
+		assertEquals(calc.soma(termoA, termoB), expected);
 	}
 
 }
