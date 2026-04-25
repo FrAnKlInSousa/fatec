a4edd149b888397f90edb550b2e8b161c555fb77
franklin <frank.fkn.23@gmail.com>
2026-04-25 13:32:54 -0300

    testes multiplicacao e subtracao

--- algoritmos/src/test/java/com/example/algoritmos/AlgoritmosApplicationTests.java
+++ algoritmos/src/test/java/com/example/algoritmos/AlgoritmosApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.algoritmos;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,8 +22,26 @@ class AlgoritmosApplicationTests {
 		"40, 23, 63",
 		"0, 0, 0"
 	})
-	void soma(float termoA, float termoB, float expected) {
-		assertEquals(calc.soma(termoA, termoB), expected);
+	void soma(float termoA, float termoB, float esperado) {
+		assertEquals(calc.soma(termoA, termoB), esperado);
 	}
 
+	@ParameterizedTest
+	@CsvSource({
+		"5, 2, 3",
+		"20, 40, -20"
+	})
+	void subrai(float termoA, float termoB, float esperado){
+		assertEquals(calc.subtrai(termoA, termoB), esperado);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"7, 8, 56",
+		"10, -3, -30",
+		"0, -3, 0"
+	})
+	void multiplica(float termoA, float termoB, float esperado){
+		assertEquals(calc.multiplica(termoA, termoB), esperado, 0.0001f);
+	}
 }
