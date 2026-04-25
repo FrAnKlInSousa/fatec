fa81bb9fd64e9a72b2052cec6067dd7432c14af6
franklin <frank.fkn.23@gmail.com>
2026-04-25 13:44:48 -0300

    teste divisao

--- algoritmos/src/test/java/com/example/algoritmos/AlgoritmosApplicationTests.java
+++ algoritmos/src/test/java/com/example/algoritmos/AlgoritmosApplicationTests.java
@@ -1,8 +1,10 @@
 package com.example.algoritmos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -43,5 +45,19 @@ class AlgoritmosApplicationTests {
 	})
 	void multiplica(float termoA, float termoB, float esperado){
 		assertEquals(calc.multiplica(termoA, termoB), esperado, 0.0001f);
+	}
+
+	@ParameterizedTest
+	@CsvSource({
+		"27, 9, 3",
+		"0, 3, 0",
+	})
+	void divide(float termoA, float termoB, float esperado){
+		assertEquals(calc.divide(termoA, termoB), esperado);
+	}
+
+	@Test
+	void dividePorZero(){
+		assertThrows(IllegalArgumentException.class, () ->{calc.divide(0, 0);});
 	}
 }
