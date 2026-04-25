e5a25b4681d00e343795037da7eeca29736eb176
franklin <frank.fkn.23@gmail.com>
2026-04-25 13:07:53 -0300

    funcao de soma

--- algoritmos/src/main/java/com/example/algoritmos/Calculadora.java
+++ algoritmos/src/main/java/com/example/algoritmos/Calculadora.java
@@ -1,5 +1,19 @@
 package com.example.algoritmos;
 
-public class Calculadora {
+import java.util.Scanner;
 
+public class Calculadora {
+    public float soma(float termoA, float termoB){
+        return termoA + termoB;
+    }
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        System.out.println("Informe os números para serem somados: ");
+        Scanner entrada = new Scanner(System.in);
+        float termoA = entrada.nextFloat();
+        float termoB = entrada.nextFloat();
+        System.out.printf("A soma entre %.2f e %.2f é = %.2f\n", termoA, termoB, calc.soma(termoA, termoB));
+
+        entrada.close();
+    }
 }
